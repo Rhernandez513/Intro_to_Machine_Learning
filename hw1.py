4a81a695d781d8ca4697f99bf93f234f5464555c
Robert David Hernandez <rherna57@mail.depaul.edu>
2021-01-24 18:01:13 -0600

    PEP 8

--- hw1.py
+++ hw1.py
@@ -1,17 +1,18 @@
 import numpy as np
 
+
 # Question 1(a)
 def prime_nums_reversed(n):
-    '''
+    """
         Write a function nums_reversed that takes in an integer n and returns a string containing all prime numbers between 1 and n in reverse order, separated by spaces. For example:
             >>> prime_nums_reversed(5)
             '5 3 2'
         Note: The ellipsis (...) indicates something you should fill in. It doesn't necessarily imply you should replace it with only one line of code.
-    '''
+    """
 
     # PUT YOUR CODE HERE
     def sieve_of_eratosthenes(n):
-        primes = [True for i in range(n+1)]
+        primes = [True for i in range(n + 1)]
         primes[0] = False
         primes[1] = False
         p = 2
@@ -28,7 +29,7 @@ def prime_nums_reversed(n):
 
 # Question 1(b)
 def string_explosion(string):
-    '''
+    """
         Write a function string_explosion that takes in a non-empty string like "Code" and returns a long string containing every suffix of the input. For example:
         >>> string_explosion('Code')
         'Codeodedee'
@@ -38,30 +39,30 @@ def string_explosion(string):
         'hii'
 
         Hint: Try to use recursion.
-    '''
+    """
 
     # PUT YOUR CODE HERE
-    pass    # remove the 'pass'
+    pass  # remove the 'pass'
 
 
 # Question 1(c)
 def replace(a, b):
-    '''
+    """
         Write a function `replace` that takes in two lists: `a` and `b`, and returns a list where the last element of `a` is replaced by the list `b`.
 
         >>> replace([1, 2, 3, 4], [5, 6, 7, 8])
         [1, 2, 3, 5, 6, 7, 8]
         >>> replace([8, 4, 3], [4, 1, 3, 0, 10])
         [8, 4, 4, 1, 3, 0, 10]
-    '''
+    """
 
     # PUT YOUR CODE HERE
-    pass    # remove the 'pass'
+    pass  # remove the 'pass'
 
 
 # Question 2(a)
 def bowl_cost(v):
-    '''
+    """
         v is the price vector
         it can take any value like this: np.array([1,3,5])
 
@@ -74,9 +75,9 @@ def bowl_cost(v):
 
         Create a 2-dimensional numpy array encoding the matrix B such that the matrix-vector multiplication
             B x v
-        evaluates to a length 4 column vector containing the price of each fruit bowl. 
+        evaluates to a length 4 column vector containing the price of each fruit bowl.
         The first entry of the result should be the cost of fruit bowl #1, the second entry the cost of fruit bowl #2, etc.
-    '''
+    """
 
     B = np.array([
         # You should fill this in.
@@ -88,7 +89,7 @@ def bowl_cost(v):
 
 # Question 2(b)
 def amount_spent(v, B):
-    '''
+    """
         v and B are from the previous question (2a).
 
         Bob, Daniela, and Luke make the following purchases:
@@ -99,28 +100,28 @@ def amount_spent(v, B):
 
         Create a matrix A such that the matrix expression
             A x B x v
-        evaluates to a length 3 column vector containing how much each of them spent. 
+        evaluates to a length 3 column vector containing how much each of them spent.
         The first entry of the result should be the total amount spent by Bob, the second entry the amount sent by Daniela, etc.
-    '''
+    """
 
     A = np.array([
         [0, 3, 2, 0]
-    ]) # Finish this!
+    ])  # Finish this!
 
     return A @ B @ v
 
 
 # Question 2(c)
 def new_price(A, B, x):
-    '''
+    """
         A and B are from previous question (2b).
 
-        Let's suppose Berkeley Bowl changes their fruit prices, but you don't know what they changed their prices to. 
-        Joey, Deb, and Sam buy the same quantity of fruit baskets and the number of fruit in each basket is the same, 
+        Let's suppose Berkeley Bowl changes their fruit prices, but you don't know what they changed their prices to.
+        Joey, Deb, and Sam buy the same quantity of fruit baskets and the number of fruit in each basket is the same,
         but now the amount they spent is given by 'x':
 
         Use np.linalg.inv and x to compute the new prices for the individual fruits:
-    '''
+    """
 
-    new_v = None    # Fill your code here
+    new_v = None  # Fill your code here
     return new_v
